f3c8c5374fe385985342778c3712bed9095f2ccc
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2022-04-29 14:22:20 +0200

    Formatting

--- index.js
+++ index.js
@@ -1,65 +1,57 @@
 class Product {
-    constructor(name, price) {
-        console.log(typeof price);
-       this.name = name;
-       this.price = price;
-    }
-    containedVAT() {
-        const taxIncluded = (this.price *0.16)
-        return `${taxIncluded.toFixed(2)} € VAT incl.`
-    }
+  constructor(name, price) {
+    this.name = name
+    this.price = price
+  }
 
+  containedVAT() {
+    const taxIncluded = this.price * 0.16
+    return `${taxIncluded.toFixed(2)} € VAT incl.`
+  }
 
-    toText() {
-        return `${this.name} ${this.price.toFixed(2)} € in total. ${this.containedVAT()} (16%).`;;
-    }
-
+  toText() {
+    return `${this.name} ${this.price.toFixed(2)} € in total. ${this.containedVAT()} (16%).`
+  }
 }
-
-//created some products for the shop
-const adidasShoes = new Product('Adidas running shoes', 150.00);
-const paintingSet = new Product('tracksuit', 99.99)
-
-console.log(adidasShoes.containedVAT());
-console.log(adidasShoes.toText());
 
 class Cart {
-    constructor () {
-        this.products = [];
-    }
-    addProduct(shoppedProduct) {
-        console.log(shoppedProduct);
-        if (shoppedProduct instanceof Product) {
-            console.log(shoppedProduct instanceof Product);
-            this.products.push(shoppedProduct)
-            console.log("these items are in your cart",this.products);
-        }else {
-            return `'Unfortunately, this product is not available in our shop!'`;
-        }
-    }
-    getProductInfoCart(){
-        const info = this.products.map(item => item.toText())
-        return info
-    }
+  constructor() {
+    this.products = []
+  }
 
-
-    getTotalPriceCart() {
-        const sum = this.products.map(element => Number(element.price)).reduce((acc, curr) => acc + curr, 0);
-        return `The total for ${this.products.length} items in your cart amounts to ${sum} €.` 
+  addProduct(shoppedProduct) {
+    if (shoppedProduct instanceof Product) {
+      this.products.push(shoppedProduct)
+      return `Your shopping cart now contains ${this.products.length} products`
+    } else {
+      return `Unfortunately, this product is not available in our shop!`
     }
-    
+  }
 
+  getProductInfoCart() {
+    return this.products.map(item => item.toText())
+  }
+
+  getTotalPriceCart() {
+    const sum = this.products.map(element => Number(element.price)).reduce((acc, curr) => acc + curr, 0)
+    return `The total for ${this.products.length} items in your cart amounts to ${sum} €.`
+  }
 }
 
-const customerOneCart = new Cart
+// Create products
+const adidasShoes = new Product("Adidas running shoes", 150.0)
+const paintingSet = new Product("tracksuit", 99.99999)
 
-//not an instance of Product class 
-// console.log(customerOneCart.addProduct({ name: 'painting brush', price: 20 }));
-// 'Unfortunately, this product is not available in our shop!'
+// Test products
+console.log(adidasShoes.toText())
+console.log(adidasShoes.containedVAT())
 
-//added products
-console.log(customerOneCart.addProduct(adidasShoes));
-console.log(customerOneCart.addProduct(paintingSet));
-console.log(customerOneCart.getTotalPriceCart());
-console.log(customerOneCart.getProductInfoCart());
+// Create cart
+const cart = new Cart()
 
+// Test cart
+console.log(cart.addProduct({ name: 'painting brush', price: 20 })) // Fails; not a product!
+console.log(cart.addProduct(adidasShoes))
+console.log(cart.addProduct(paintingSet))
+console.log(cart.getTotalPriceCart())
+console.log(cart.getProductInfoCart())
